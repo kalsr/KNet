6302ba40288704046f8e90665e5f46e23ae93f39
kalsr <42611658+kalsr@users.noreply.github.com>
2025-09-08 12:43:51 -0400

    Add files via upload

---
+++ Supply_Chain_Risk_Estimator.py
@@ -0,0 +1,22 @@
+# LLL-PYTHON
+
+import streamlit as st
+
+
+
+# Your actual program link
+
+program_link = "https://k8kopgxw6rayvcezgdjh5u.streamlit.app/"
+
+
+
+# Show it with a meaningful name
+
+st.markdown(
+
+    f"[Supply-Chain-Risk-Estimator]({program_link})",
+
+    unsafe_allow_html=True
+
+)
+
---
+++ app.py
@@ -0,0 +1,11 @@
+# AAA-PYTHON
+
+# python
+import streamlit as st
+
+program_link = "https://k8kopgxw6rayvcezgdjh5u.streamlit.app/"
+
+st.markdown(
+    f"[🚀 Supply-Chain-Risk-Estimator App]({program_link})",
+    unsafe_allow_html=True
+)
