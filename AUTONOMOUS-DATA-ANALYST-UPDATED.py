cf2d41d2eebbd2a55701f4641de9b915f387dfb5
kalsr <42611658+kalsr@users.noreply.github.com>
2025-09-29 12:47:53 -0400

    Update AUTONOMOUS-DATA-ANALYST-UPDATED.py

--- AUTONOMOUS-DATA-ANALYST-UPDATED.py
+++ AUTONOMOUS-DATA-ANALYST-UPDATED.py
@@ -170,8 +170,8 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-st.title("Autonomous Data Analyst Designed & Developed By Randy Singh.")
-st.write("An Orchestrator which Coordinates Small Specialized Agentic-AI Agents (Ingestor, Cleaner, Summarizer, Anomaly Detector, Segmenter, Modeler, Reporter).")
+st.title("Agentic-AI Autonomous Data Analyst- Developed By Randy Singh.")
+st.write("An Orchestrator which Coordinates Small Specialized Autonomus Agents (Ingestor, Cleaner, Summarizer, Anomaly Detector, Segmenter, Modeler, Reporter).")
 
 # Top controls
 col1, col2, col3 = st.columns([2,1,1])
@@ -330,6 +330,7 @@ for k, v in st.session_state["report"].items():
             st.write("-", item)
     else:
         st.write(v)
+
 
 
 
