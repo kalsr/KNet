84d843f703fcc4c5dad3d33f66c961dcaf4447ec
kalsr <42611658+kalsr@users.noreply.github.com>
2025-12-15 12:02:43 -0500

    Update Fraud-Scam-Detector.py

--- Fraud-Scam-Detector.py
+++ Fraud-Scam-Detector.py
@@ -221,7 +221,7 @@ def add_result(src, content):
 
     res, score = detect_fraud(content)
 
-    st.session_state.results.loc[len(st.session_state.results)] = \[src, content, score, res]
+    st.session_state.results.loc[len(st.session_state.results)] = \(src, content, score, res)
 
         
 
@@ -520,5 +520,6 @@ st.markdown(
     "© KNet Consulting Group | Enterprise Fraud & Scam Detection Platform"
 
 )
+
 
 
