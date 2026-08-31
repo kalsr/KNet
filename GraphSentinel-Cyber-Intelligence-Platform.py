58bde52566ac45a983e4a69c4f49443f02d354f3
kalsr <42611658+kalsr@users.noreply.github.com>
2026-08-31 18:27:02 -0400

    Update GraphSentinel-Cyber-Intelligence-Platform.py

--- GraphSentinel-Cyber-Intelligence-Platform.py
+++ GraphSentinel-Cyber-Intelligence-Platform.py
@@ -1198,7 +1198,7 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 st.set_page_config(
-    page_title="GraphSentinel Defense and Cyber Intelligence Platform",
+    page_title="GraphTheory Defense and Cyber Intelligence Platform",
     page_icon=None,
     layout="wide",
 )
